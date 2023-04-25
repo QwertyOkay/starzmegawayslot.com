2d910ef0d105910b5e4b7527d66e6139ef77bb81
Alexandr Kotenko <60352683+QwertyOkay@users.noreply.github.com>
2023-04-25 17:25:30 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 (() => {
     const refs = {
 
+      openModalBtn: document.querySelector('[data-modal-open]'),
+      closeModalBtn: document.querySelector('[data-modal-close]'),
+      modal: document.querySelector('[data-modal]'),
+
       openModalBtn2: document.querySelector('[data-modal-open2]'),
       closeModalBtn2: document.querySelector('[data-modal-close2]'),
       modal2: document.querySelector('[data-modal2]'),
@@ -11,12 +15,19 @@
 
     };
 
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+
     refs.openModalBtn2.addEventListener('click', toggleModal2);
     refs.closeModalBtn2.addEventListener('click', toggleModal2);
 
     refs.openModalBtn3.addEventListener('click', toggleModal3);
     refs.closeModalBtn3.addEventListener('click', toggleModal3);
 
+  
+  function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+    }
 
     function toggleModal2() {
       refs.modal2.classList.toggle('is-hidden2');
